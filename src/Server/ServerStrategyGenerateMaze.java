97e1219e8f7d250a8efe710d2f7d794d9b1e2a62
ayemoy <ayelet431@gmail.com>
2021-05-03 21:24:41 +0300

    do ServerStrategyGenerateMaze

--- src/Server/ServerStrategyGenerateMaze.java
+++ src/Server/ServerStrategyGenerateMaze.java
@@ -1,7 +1,11 @@
 package Server;
 
-import java.io.InputStream;
-import java.io.OutputStream;
+import IO.MyCompressorOutputStream;
+import algorithms.mazeGenerators.AMazeGenerator;
+import algorithms.mazeGenerators.Maze;
+import algorithms.mazeGenerators.MyMazeGenerator;
+
+import java.io.*;
 
 //this class implement IServerStrategy interface and must do the applyStrategy function like they want in the workPapers
 
@@ -14,13 +18,26 @@ public class ServerStrategyGenerateMaze implements IServerStrategy{
 
         try
         {
+            ObjectInputStream readFromClient = new ObjectInputStream(inputFromClient); //read from here the array with the maze details
+            ObjectOutputStream sendToClient = new ObjectOutputStream(outputToClient); //return to client the compressed maze object
+
+            //reads from client
+            int[] mazeArraySize = (int[]) readFromClient.readObject(); //in this array we will save the size of the maze that the client senf us
+
+            //generates the maze
+            AMazeGenerator mazeGenerator; //create a mazegenerator instance
+            mazeGenerator = new MyMazeGenerator(); //open a new mazegenerator
+            Maze maze = mazeGenerator.generate(mazeArraySize[0], mazeArraySize[1]); //open maze object in the right size
+
+            //then we compresses the maze so we can send it back to client
+            OutputStream out = new MyCompressorOutputStream(sendToClient);
+            out.write(maze.toByteArray());
+
+            out.flush();
+            out.close();
 
         }
-
-        catch ()
-        {
-
-        }
+         catch (IOException | ClassNotFoundException e){e.printStackTrace();}
 
     }
 
