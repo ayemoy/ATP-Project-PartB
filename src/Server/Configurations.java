d2f0041ab11b9dec01d27a532e748530867d24f5
natkat94 <82222809+natkat94@users.noreply.github.com>
2021-05-09 15:23:52 +0300

    Update Configurations.java

--- src/Server/Configurations.java
+++ src/Server/Configurations.java
@@ -7,12 +7,12 @@ import java.io.*;
 public class Configurations {
 
     public static String getProperty (String str) {
-        try (InputStream input = Configurations.class.getClassLoader().getResourceAsStream("config.properties"))
+        try (InputStream input = Configurations.class.getClassLoader().getResourceAsStream("config.properties"))//trying to get the properties from config file, 
         {
-
+            
             Properties newProper = new Properties();
 
-            if (input == null) {return null;}
+            if (input == null) {return null;}// if null it means that resource could not be found
 
             //load a properties file from class path, inside static method
             newProper.load(input);
